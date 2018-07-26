c568673591e0b75a5e29e9994a9b7e65a8fda6b3
google <google@maswadkar.com>
2018-07-26 07:03:22 +0000

    added app.yaml

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const Datastore = require('@google-cloud/datastore');
+
+// Instantiate a datastore client
+const datastore = Datastore();
+
+const query = datastore.createQuery('student').limit(10);
 
 app = express()
 
 app.listen(8080)
 
+
 app.get('/',function(req,res){res.json({"message":"Hello World"})})
+
+app.get('/students',function(req,res){
+    datastore.runQuery(query).then(function(results){
+	console.log(results)
+	res.status(200).json(results)
+    });
+})
