8b94729316bf473a0b9c250b7968a9888055470a
vk <google@maswadkar.com>
2018-08-14 14:06:37 +0000

    added Router for students

--- app.js
+++ app.js
@@ -3,43 +3,19 @@ const bodyParser = require('body-parser');
 const Datastore = require('@google-cloud/datastore');
 
 const questions = require('./questions')
-
+const students = require('./students')
 
 // Instantiate a datastore client
 const datastore = Datastore();
 
-const query = datastore.createQuery('student')
+const query_students = datastore.createQuery('student')
 const query_questions = datastore.createQuery('question')
 
 app = express()
 app.listen(8080)
 app.use(bodyParser.json())
 app.use('/questions',questions)
+app.use('/students',students)
 
 app.get('/',function(req,res){res.json({"message":"Hello World"})})
 app.get('/ping',function(req,res){res.json({"message":"pinging everywhere"})})
-
-
-//STUDENT
-app.get('/students',function(req,res){
-    datastore.runQuery(query).then(function(results){
-	res.status(200).json(results)
-    });
-})
-
-app.post('/students',function(req,res){
-	console.log(req.body)
-	const entity = {  key:{kind:"student"},data:req.body}
-	datastore.insert(entity).then( function(){res.status(200).json("inserted succesfully")});
-})
-
-
-
-//QUESTION
-
-app.get('/questions',function(req,res){
-    datastore.runQuery(query_questions).then(function(results){
-        console.log(results)
-        res.status(200).json(results)
-    });
-})
--- questions.js
+++ questions.js
@@ -1,15 +1,36 @@
-var express = require('express')
-var questions = express.Router()
+const express = require('express')
+const Datastore = require('@google-cloud/datastore');
+
+const questions = express.Router();
+
+const datastore = Datastore();
+const query_questions = datastore.createQuery('question')
 
 // middleware that is specific to this router
 questions.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now())
   next()
 })
-// define the home page route
-questions.get('/', function (req, res) {
-  res.send('Birds home page')
+
+
+//QUESTION
+
+questions.get('/',function(req,res){
+    datastore.runQuery(query_questions).then(function(results){
+        console.log(results)
+        res.status(200).json(results)
+    });
 })
+
+
+questions.post('/',function(req,res){
+        console.log(req.body)
+        const entity = {  key:{kind:"question"},data:req.body}
+        datastore.insert(entity).then( function(){res.status(200).json("inserted succesfully")});
+})
+
+
+
 // define the about route
 questions.get('/about', function (req, res) {
   res.send('About birds')
