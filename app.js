9037fcbee350079408bc2c2fd177b367355e5673
maswadkar <maswadkar@gmail.com>
2018-08-14 17:16:18 +0200

    working on macbook now

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ const Datastore = require('@google-cloud/datastore');
 const questions = require('./questions')
 const students = require('./students')
 
-
-// Instantiate a datastore client
-const datastore = Datastore();
-const query_students = datastore.createQuery('student')
-const query_questions = datastore.createQuery('question')
-
-
 app = express()
 app.listen(8080)
 app.use(bodyParser.json())
