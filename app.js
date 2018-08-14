b31142dee1f569ccb04c6321c91d3397758a82e8
vk <google@maswadkar.com>
2018-08-14 13:41:35 +0000

    implementation of express.Routers

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const Datastore = require('@google-cloud/datastore');
 
+const questions = require('./questions')
+
+
 // Instantiate a datastore client
 const datastore = Datastore();
 
@@ -11,7 +14,7 @@ const query_questions = datastore.createQuery('question')
 app = express()
 app.listen(8080)
 app.use(bodyParser.json())
-
+app.use('/questions',questions)
 
 app.get('/',function(req,res){res.json({"message":"Hello World"})})
 app.get('/ping',function(req,res){res.json({"message":"pinging everywhere"})})
