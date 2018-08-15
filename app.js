3bda0e09f4bc9d8b6a76be25e760c5f09dae33ca
vk <google@maswadkar.com>
2018-08-15 11:50:53 +0000

    implemented quiz and response in app js

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const Datastore = require('@google-cloud/datastore');
 
 const questions = require('./questions')
 const students = require('./students')
+const quiz = require('./quiz')
+const response = require('./response')
 
 app = express()
 app.listen(8080)
 app.use(bodyParser.json())
 app.use('/questions',questions)
 app.use('/students',students)
+app.use('/quiz',quiz)
+app.use('/response',response)
+
 
 app.get('/',function(req,res){res.json({"message":"Hello World"})})
 app.get('/ping',function(req,res){res.json({"message":"pinging everywhere"})})
